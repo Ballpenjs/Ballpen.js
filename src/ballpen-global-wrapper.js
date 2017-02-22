96f27635dbe3827fa65a90d273486d10bd1443c0
JasonYu <yuhang@tapatalk.com>
2017-02-22 14:33:07 +0800

    Update plugins support.

--- src/ballpen-global-wrapper.js
+++ src/ballpen-global-wrapper.js
@@ -1,11 +1,19 @@
 import Axios from 'axios';
+import Underscore from 'underscore';
+
+// Ballpen build-in plugins
+// import Cookie from 'ballpen-plugin-cookie';
+// import Cache from 'ballpen-plugin-cache';
 
 class BallpenGlobalWrapper {
     static set(Ballpen) {
         Ballpen.registerPlugin = BallpenGlobalWrapper.registerPlugin;
 
-        // Build-in plugins
+        // Bind plugins
         Ballpen.$http = Axios;
+        Ballpen.$util = Underscore;
+        // Ballpen.$cache = Cache;
+        // Ballpen.$cookie = Cookie;
 
         // Global variables
         Ballpen.$refs = {};
