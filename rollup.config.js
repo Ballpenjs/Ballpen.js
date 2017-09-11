0627dad6a3e0b5bbcfab4e62094e21abc1f207a2
Jason <yhorg@hotmail.com>
2017-09-11 17:52:22 +0800

    Remove plugin system.

--- rollup.config.js
+++ rollup.config.js
@@ -5,32 +5,38 @@ import filesize from 'rollup-plugin-filesize';
 import uglify from 'rollup-plugin-uglify';
 import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
+import builtins from 'rollup-plugin-node-builtins';
+import json from 'rollup-plugin-json';
 
 // Source and output files name
 let buildDistPathFile = path.resolve(__dirname, './dist/ballpen.min.js');
 let sourcePathFile = path.resolve(__dirname, './src/ballpen.js');
 
 export default {
-  	entry: sourcePathFile,
+	entry: sourcePathFile,
 	plugins: [
 		eslint(),
-	    babel({
-	      exclude: '../node_modules/**',
-	      runtimeHelpers: true,
-	    }),
-	    nodeResolve({ 
-	      jsnext: true, 
-	      main: true 
-	    }),
-	    // Convert CommonJS modules to ES2015
-	    commonjs(),
-	    // Compress dist file
-	    uglify(),
-	    // Show generated file size in cli
-	    filesize()
+		babel({
+			exclude: '../node_modules/**',
+			runtimeHelpers: true
+		}),
+		nodeResolve({
+			jsnext: true,
+			main: true,
+            preferBuiltins: false,
+            browser: true
+		}),
+		// Convert CommonJS modules to ES2015
+		commonjs(),
+		// Compress dist file
+		uglify(),
+		// Show generated file size in cli
+		filesize(),
+        builtins(),
+		json()
 	],
-	format: "umd",
-    moduleName: "Ballpen",
+	format: 'umd',
+    moduleName: 'Ballpen',
     dest: buildDistPathFile,
     sourceMap: true
 };
--- src/ballpen-global-wrapper.js
+++ src/ballpen-global-wrapper.js
@@ -1,11 +1,3 @@
-import Axios from 'axios';
-import Underscore from 'underscore';
-import Velocity from 'velocity-animate';
-
-// Ballpen build-in plugins
-import Cookie from 'ballpen-plugin-cookie';
-import Cache from 'ballpen-plugin-cache';
-
 // Ballpen wasm loader
 import Wasm from './ballpen-wasm.js';
 
@@ -13,12 +5,6 @@ class BallpenGlobalWrapper {
     static set(Ballpen) {
         Ballpen.registerPlugin = BallpenGlobalWrapper.registerPlugin;
 
-        // Bind plugins
-        Ballpen.$http = Axios;
-        Ballpen.$util = Underscore;
-        Ballpen.$cache = Cache;
-        Ballpen.$cookie = Cookie;
-        Ballpen.$animate = Velocity;
 
         // Global variables
         Ballpen.$refs = {};
